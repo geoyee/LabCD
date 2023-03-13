e513c7c9d0e6131eee101f17ea65ab6a377d9356
geoyee <geoyee@yeah.net>
2023-03-13 16:16:49 +0800

    Update i18n

--- labcd/i18n/English.ts
+++ labcd/i18n/English.ts
@@ -30,33 +30,38 @@
     </message>
     <message>
         <location filename="../labcd.cpp" line="56"/>
+        <source>从标签建立标注</source>
+        <translation>Create annotation from mask</translation>
+    </message>
+    <message>
+        <location filename="../labcd.cpp" line="59"/>
         <source>关于</source>
         <translation>About</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="58"/>
+        <location filename="../labcd.cpp" line="61"/>
         <source>github主页</source>
         <translation>Homepage of github</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="63"/>
+        <location filename="../labcd.cpp" line="66"/>
         <source>使用帮助</source>
         <translation>Help</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="71"/>
+        <location filename="../labcd.cpp" line="74"/>
         <source>语言</source>
         <translation>Language</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="76"/>
-        <location filename="../labcd.cpp" line="93"/>
+        <location filename="../labcd.cpp" line="78"/>
+        <location filename="../labcd.cpp" line="92"/>
         <source>英文</source>
         <translation>English</translation>
     </message>
     <message>
         <location filename="../labcd.cpp" line="81"/>
-        <location filename="../labcd.cpp" line="87"/>
+        <location filename="../labcd.cpp" line="86"/>
         <source>中文</source>
         <translation>Chinese</translation>
     </message>
@@ -67,103 +72,103 @@
         <translation>Location: </translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="124"/>
+        <location filename="../labcd.cpp" line="123"/>
         <source>数据列表</source>
         <translation>Data List</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="135"/>
+        <location filename="../labcd.cpp" line="136"/>
         <source>加载图像：</source>
         <translation>Loaded: </translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="141"/>
+        <location filename="../labcd.cpp" line="142"/>
         <source>标签列表</source>
         <translation>Label List</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="148"/>
+        <location filename="../labcd.cpp" line="149"/>
         <source>当前标签：[</source>
         <translation>Label: [</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="170"/>
+        <location filename="../labcd.cpp" line="178"/>
         <source>保存</source>
         <translation>Save</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="175"/>
+        <location filename="../labcd.cpp" line="183"/>
         <source>上一张</source>
         <translation>Last image</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="182"/>
+        <location filename="../labcd.cpp" line="190"/>
         <source>下一张</source>
         <translation>Next image</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="190"/>
+        <location filename="../labcd.cpp" line="198"/>
         <source>放大</source>
         <translation>Enlarge</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="195"/>
+        <location filename="../labcd.cpp" line="203"/>
         <source>缩小</source>
         <translation>Narrow</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="200"/>
+        <location filename="../labcd.cpp" line="208"/>
         <source>全幅缩放</source>
         <translation>Full</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="212"/>
+        <location filename="../labcd.cpp" line="217"/>
         <source>删除多边形</source>
         <translation>Delete polygon</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="222"/>
+        <location filename="../labcd.cpp" line="227"/>
         <source>删除所有多边形</source>
         <translation>Delete all polygons</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="230"/>
-        <location filename="../labcd.cpp" line="425"/>
+        <location filename="../labcd.cpp" line="235"/>
+        <location filename="../labcd.cpp" line="452"/>
         <source>设置十字丝颜色</source>
         <translation>Set cross color</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="234"/>
+        <location filename="../labcd.cpp" line="239"/>
         <source>打开变化参考图</source>
         <translation>Open reference image</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="241"/>
+        <location filename="../labcd.cpp" line="246"/>
         <source>光谱变化向量强度参考图</source>
         <translation>CVA intensity reference</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="266"/>
+        <location filename="../labcd.cpp" line="271"/>
         <source>LabCD - 遥感变化检测标注工具</source>
         <translation>LabCD - Remote sensing change detection annotation tool</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="307"/>
+        <location filename="../labcd.cpp" line="311"/>
         <source>打开大图像</source>
         <translation>Open large image</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="309"/>
+        <location filename="../labcd.cpp" line="313"/>
         <source>栅格图像文件 (*.tif *.tiff)</source>
         <translation>Raster File (*.tif *.tiff)</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="322"/>
+        <location filename="../labcd.cpp" line="325"/>
         <source>设置</source>
         <translation>Setting</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="323"/>
+        <location filename="../labcd.cpp" line="326"/>
         <source>设置切块大小</source>
         <translation>Set block size</translation>
     </message>
@@ -173,27 +178,32 @@
         <translation>Split finished, save to: </translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="338"/>
+        <location filename="../labcd.cpp" line="336"/>
         <source>切分失败，可能是不支持的类型或超出范围的切块大小</source>
         <translation>Split failed, it may be an unsupported type or an out-of-range block size</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="359"/>
+        <location filename="../labcd.cpp" line="353"/>
         <source>合并完成，保存至：</source>
         <translation>Merge finished, save to: </translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="363"/>
+        <location filename="../labcd.cpp" line="355"/>
         <source>合并失败</source>
         <translation>Merge failed</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="396"/>
+        <location filename="../labcd.cpp" line="368"/>
         <source>清理完成</source>
         <translation>Clear finished</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="416"/>
+        <location filename="../labcd.cpp" line="408"/>
+        <source>转换完成</source>
+        <translation>Convert finished</translation>
+    </message>
+    <message>
+        <location filename="../labcd.cpp" line="443"/>
         <source>保存图像：</source>
         <translation>Saved: </translation>
     </message>
@@ -201,17 +211,17 @@
 <context>
     <name>LabelTable</name>
     <message>
-        <location filename="../widgets/labeltable.cpp" line="47"/>
+        <location filename="../widgets/labeltable.cpp" line="45"/>
         <source>添加标签</source>
         <translation>Add label</translation>
     </message>
     <message>
-        <location filename="../widgets/labeltable.cpp" line="69"/>
+        <location filename="../widgets/labeltable.cpp" line="67"/>
         <source>标签颜色选择</source>
         <translation>Label color selection</translation>
     </message>
     <message>
-        <location filename="../widgets/labeltable.cpp" line="141"/>
+        <location filename="../widgets/labeltable.cpp" line="142"/>
         <source>背景</source>
         <translation>Background</translation>
     </message>
@@ -219,18 +229,18 @@
 <context>
     <name>MultCanvas</name>
     <message>
-        <location filename="../widgets/multcanvas.cpp" line="73"/>
-        <location filename="../widgets/multcanvas.cpp" line="83"/>
+        <location filename="../widgets/multcanvas.cpp" line="72"/>
+        <location filename="../widgets/multcanvas.cpp" line="80"/>
         <source>错误</source>
         <translation>Error</translation>
     </message>
     <message>
-        <location filename="../widgets/multcanvas.cpp" line="74"/>
+        <location filename="../widgets/multcanvas.cpp" line="73"/>
         <source>无法打开图像文件。</source>
         <translation>Unable to open image file.</translation>
     </message>
     <message>
-        <location filename="../widgets/multcanvas.cpp" line="84"/>
+        <location filename="../widgets/multcanvas.cpp" line="81"/>
         <source>两个时段的数据大小不一致。</source>
         <translation>The data size of the two periods is inconsistent.</translation>
     </message>
@@ -239,35 +249,40 @@
     <name>QObject</name>
     <message>
         <location filename="../utils/fileworker.cpp" line="15"/>
-        <location filename="../labcd.cpp" line="347"/>
+        <location filename="../labcd.cpp" line="344"/>
         <source>打开图像文件夹</source>
         <translation>Open image folder</translation>
     </message>
     <message>
-        <location filename="../utils/fileworker.cpp" line="33"/>
-        <location filename="../utils/fileworker.cpp" line="52"/>
+        <location filename="../utils/fileworker.cpp" line="31"/>
+        <location filename="../utils/fileworker.cpp" line="50"/>
         <source>错误</source>
         <translation>Error</translation>
     </message>
     <message>
-        <location filename="../utils/fileworker.cpp" line="34"/>
+        <location filename="../utils/fileworker.cpp" line="32"/>
         <source>文件组织格式错误，请确保数据文件夹下仅存在两个（或三个）子文件夹，分别为A、B（以及GT）。</source>
         <translation>The file organization format is incorrect. Please ensure that there are only two (or three) subfolders under the data folder, namely A, B (and GT).</translation>
     </message>
     <message>
-        <location filename="../utils/fileworker.cpp" line="53"/>
+        <location filename="../utils/fileworker.cpp" line="51"/>
         <source>时段一数据和时段二数据数量不相等。</source>
         <translation>The number of data in period 1 and period 2 is not equal.</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="99"/>
+        <location filename="../labcd.cpp" line="98"/>
         <source>提示</source>
         <translation>Infomation</translation>
     </message>
     <message>
-        <location filename="../labcd.cpp" line="100"/>
+        <location filename="../labcd.cpp" line="99"/>
         <source>重启软件后更新语言设置。</source>
         <translation>Update the language settings after restarting the software.</translation>
+    </message>
+    <message>
+        <location filename="../labcd.cpp" line="401"/>
+        <source>打开标签文件夹</source>
+        <translation>Open mask folder</translation>
     </message>
 </context>
 </TS>
